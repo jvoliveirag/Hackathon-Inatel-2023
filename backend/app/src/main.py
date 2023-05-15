968e19177a0a2502d8470f46d6d41d7ff04557a1
Maycol <maycolteles@hotmail.com>
2023-05-15 20:44:49 -0300

    feat: creating the models package and adding a scratch for user class. also removing unused code from the main module.

--- backend/app/src/main.py
+++ backend/app/src/main.py
@@ -1,11 +1,7 @@
 """
 Module containing the "main()" function.
-
-DOING: MAKING USE CASE AVAILABLE TO THE UI
 """
 
-# TODO: REMOVE LINE BELOW
-import test_functions
 from dependencies import UI_INJECTION
 
 
@@ -13,8 +9,6 @@ def main() -> None:
     """
     Main Function. This is where the application starts.
     """
-    print("Hello, World!")
-    # test_functions.test_use_case()
     UI_INJECTION.execute()
 
 
