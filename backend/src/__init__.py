1a549bbb011f389e912165a8dd35f7048a561249
Maycol <maycolteles@hotmail.com>
2023-05-15 20:04:18 -0300

    refactor: renaming the folder.s

--- backend/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-__init__ file to export the classes below.
-"""
-
-__all__ = ["main"]
-
-
-from .main import main
--- backend/src/application/infra/frameworks/sockets/constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Module containing all the socket constants.
-"""
-
-SERVER_HOST = "127.0.0.1"
-
-SERVER_PORT = 50_000
-
-SERVER_ADDRESS = (SERVER_HOST, SERVER_PORT)
