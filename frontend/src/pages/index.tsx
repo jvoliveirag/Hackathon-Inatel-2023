aaf337cd0a3feb535bfe38f958abdbed03ff6087
jvoliveirag <jv.oliveira@gmail.com>
2023-05-16 13:48:03 -0300

    style | feat: adding the bar to the main page.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
+import { NavBar } from '../components/NavBar'
 
 export default function Home() {
 
@@ -12,11 +13,13 @@ export default function Home() {
         
       <div className='h-screen flex flex-col'>  
 
+        <NavBar linkName={['Notificações', 'Ajuda', 'Contato']} linkPath={['#', '#', '#']} />
+
         <main className='flex items-center justify-center flex-wrap flex-col flex-1 pb-10'>
           
         </main>
 
-        <footer className='flex items-center justify-center text-sm align-baseline text-white'>
+        <footer className='flex items-center justify-center text-xs align-baseline text-white'>
           <p>₢ 2023 nTracker | Todos os direitos reservados</p>
         </footer>
 
